a4b24652f0bfbb4d7c88b5b6d4e487b59faa83c2
Алексей <98337207+AlexConglomerate@users.noreply.github.com>
2022-01-27 23:01:02 +0500

    Add files via upload

---
+++ Первый сайт/code.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    let num1 = document.getElementById("myId")
+        // num1.innerHTML = "nnihnuindiuancuidf"
+
+    // alert(num1.textContent)
+
+    for (let i = 0; i < 5; i++) {
+        num1.innerHTML = num1.textContent + i + '<br>'
+    }
+
+    console.log(999)
+        // let num2 = document.getElementById("num2").value
+        // document.getElementById("sum").value = Number(num1) + Number(num2)
+}
